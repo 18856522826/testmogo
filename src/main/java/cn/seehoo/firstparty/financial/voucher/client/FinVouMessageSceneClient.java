e66597f79602c0150cddd332d440f0a1c47519cc
yuanxin_liu@outlook.com <yuanxin_liu@outlook.com>
2021-03-16 14:48:28 +0800

    添加构造函数

--- src/main/java/cn/seehoo/firstparty/financial/voucher/client/FinVouMessageSceneClient.java
+++ src/main/java/cn/seehoo/firstparty/financial/voucher/client/FinVouMessageSceneClient.java
@@ -2,6 +2,7 @@ package cn.seehoo.firstparty.financial.voucher.client;
 
 import cn.seehoo.firstparty.financial.voucher.model.MarginCollectionMessage;
 import cn.seehoo.firstparty.financial.voucher.model.VoucherStandardMessage;
+import lombok.AllArgsConstructor;
 
 /**
  * Notice: 财务凭证信息场景客户端：提供各场景消息转换、组装及发送功能
@@ -11,8 +12,9 @@ import cn.seehoo.firstparty.financial.voucher.model.VoucherStandardMessage;
  * @date 2021/3/15
  * @since 1.0
  */
+@AllArgsConstructor
 public class FinVouMessageSceneClient {
-    private FinVouMessageSender sender;
+    private final FinVouMessageSender sender;
     /**
      * 保证金收取场景
      * @param message 当前场景业务信息
