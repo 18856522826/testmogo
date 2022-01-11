9d1d03151b2278d93961618b34eeb591af0dbe05
xuxinhui <xuxh@seehoo.cn>
2022-01-11 17:10:35 +0800

    [--bug=1012546]feat(service):   收取保证金TransType取值不对b口径应为0050301

--- src/main/java/cn/seehoo/firstparty/financial/voucher/service/FinVouBService.java
+++ src/main/java/cn/seehoo/firstparty/financial/voucher/service/FinVouBService.java
@@ -57,7 +57,7 @@ public class FinVouBService implements FinVouService {
         List<AcctDocGenTransDoc> docList = new ArrayList<>();
         AcctDocGenTransDoc transDoc = new AcctDocGenTransDoc();
         transDoc.setSubTransName(ClientConstants.SUB_TRANS_NAME_MARGIN);
-        transDoc.setTransType(ClientConstants.TRANS_TYPE_MARGIN_B);
+        transDoc.setTransType(ClientConstants.TRANS_TYPE_MARGIN);
         transDoc.setAmount(BigDecimal.ZERO);
         transDoc.setPaymentId(ClientConstants.PAYMENT_ID_ZERO);
         transDoc.setPayerBankName(message.getPayerBankName());
